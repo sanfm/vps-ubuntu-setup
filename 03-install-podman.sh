81eac582a64b369b828564f4c06bb6f9d2553470
sanfm <88844975+sanfm@users.noreply.github.com>
2026-02-14 10:19:21 +0100

    comments

--- 03-install-podman.sh
+++ 03-install-podman.sh
@@ -8,5 +8,10 @@ echo "=== Instalando Podman ==="
 apt-get update
 apt-get -y install podman podman-compose
 
+
 echo ""
 echo "✅ Podman instalado"
+echo "verifica estado"
+echo "systemctl --user status podman.socket"
+echo "para que arranque al inicio"
+echo "systemctl --user enable --now podman.socket"
